d5dff55a718606684718538aa38d154384dda7ef
emackinnon1 <emackinnon100390@gmail.com>
2020-03-19 16:09:34 -0600

    Game is working

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 const http = require('http');
 let app = http.createServer();
-const Game = require('../src/Game');
+const Game = require('./src/Game');
+const data = require('./src/data');
+const prototypeQuestions = data.prototypeData;
 
 // Start the server on port 3000
+const game = new Game();
+game.start(prototypeQuestions);
 app.listen(3000, '127.0.0.1');
 console.log('Node server running on port 3000');
-const game = new Game();
-game.start();
 
-// npm node index.js
+// node index.js
